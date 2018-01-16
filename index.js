9c7a3a23e6b9f627ce5559107892c4ad907830b9
SDS 800 <jeremyhyman@gmail.com>
2018-01-16 16:50:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function takeANumber(katzDeliLine, newName){
+ 
+ katzDeliLine.push(newName)
+  
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
+  
+}
+function nowServing(katzDeliLine){
+  
+  if (katzDeliLine.length === 0){
+   return `There is nobody waiting to be served!` 
+  }
+  else{var i = 0
+  do {
+  return `Currently serving ${katzDeliLine.shift()}.`;
+} 
+  while (i < katzDeliLine.length);{
+   
+    i++
+  }
+  }
+
+}
+
+function currentLine(line){
+  
+  if (line.length === 0){
+    return "The line is currently empty."
+  }
+  else{
+    for (var i = 1; i < line.length; i++) {
+  return `The line is currently: ${line}`;
+}
+   
+  }
+}
