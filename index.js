faddc2408fad9c875f7d15f40afc69cd5b39c4aa
SDS 800 <jeremyhyman@gmail.com>
2018-01-16 17:16:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function currentLine(line){
     return "The line is currently empty."
   }
   else{
+    let countedLine[]
     for (var i = 0; i < line.length; i++) {
   countedLine.push(`${++i}. ${line[i]}`)
 }
