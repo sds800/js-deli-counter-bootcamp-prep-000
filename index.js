4b79224bf23e0b512af3aef55edbd9f98a18596c
SDS 800 <jeremyhyman@gmail.com>
2018-01-16 17:04:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function currentLine(line){
     return "The line is currently empty."
   }
   else{
-    for (var i = 1; i < line.length; i++) {
+    for (var i = 0; i < line.length; i++) {
   return `The line is currently: ${line}`;
 }
    
